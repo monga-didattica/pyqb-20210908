6534c30300a9cc9363b95b41687ce48a69049e96
Mattia Monga <mattia.monga@unimi.it>
2021-09-08 12:31:59 +0200

    Solution

--- exam.py
+++ exam.py
@@ -5,9 +5,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.5
+#       jupytext_version: 1.11.4
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
@@ -29,6 +29,7 @@ import pandas as pd  # type: ignore
 import matplotlib.pyplot as plt # type: ignore
 import pymc3 as pm   # type: ignore
 
+
 # Your research team is studing a new protein, the *exambirulobin*, and they model its trend over time with the following differential equation.
 #
 # $  \frac{dy}{dt} = a + b\cdot y + c\cdot y^2 $
@@ -44,52 +45,108 @@ import pymc3 as pm   # type: ignore
 #
 # To get the full marks, you should declare correctly the type hints (the signature of the function) and add a doctest string.
 
-pass
+def deriv(y: float, a: float = 0.1, b: float = 0.02, c: float = 0.03) -> float:
+    """Returns the exambirubolin change over time as a function of exambirubolin value.  
+    """
+    return a + b*y + b*y**2
 
-# ### Exercise 2 (max 5 points)
+
+# ### Exercise 2 (max 7 points)
 #
 #
 # Define a function to approximate the solution of the differential equation by leveraging on the [Euler method](https://en.wikipedia.org/wiki/Euler_method), considering that $\lim_{\Delta t \to 0} f(t + \Delta t) = f(t) + dt\cdot\frac{df}{dt}$. The function should take also the initial value for $t = 0$.
 #
 # To get the full marks, you should declare correctly the type hints (the signature of the function) and add a doctest string.
 
-pass
+# +
+from typing import Callable
+    
+def approx_euler(t: np.ndarray, f0: float, dfun: Callable[[float], float]) -> np.ndarray:
+    """Compute the Euler approximation of a function on times t, with derivative dfun.
+    """
+    res = np.zeros(len(t))
+    res[0] = f0
+
+    for i in range(1, len(t)):
+        res[i] = res[i-1] + (t[i]-t[i-1])*dfun(res[i-1])
+
+    return res
+
+
+# -
 
 # ### Exercise 3 (max 3 points)
 #
 # Plot the approximate solution, with a $\Delta t = .01$ on $0 \leq t < 15$. The value at $t = 0$ is $0.1$.
 
-pass
+t = np.arange(0, 15, .01)
+fig, ax = plt.subplots()
+ax.plot(t, approx_euler(t, 0.1, lambda x: deriv(x)), label='Derivative approximation')
+_ = fig.legend()
 
-# ### Exercise 4 (max 7 points)
+# ### Exercise 4 (max 5 points)
 #
-# Experiments on *exambirulobin* samples resulted in the values stored in a file `values.csv` (first column). The corresponding measurements of the Calcium in the cell were recorded in the second column of `values.csv`
+# Experiments on *exambirulobin* samples resulted in the values stored in a file `values.csv` (first column). The corresponding measurements of the Calcium in the cell were recorded in the second column of `values.csv`. Read the values in the first column in a numpy ndarray.
 
-pass
+# +
+
+values = []
+with open('values.csv') as data_file:
+    for line in data_file:
+        values.append(float(line.split(',')[0]))
+        
+        
+exambirulobin = np.array(values)
+
+exambirulobin
+# -
 
 # ### Exercise 5 (max 2 points)
 #
 # Define a Pandas DataFrame with the data in `values.csv` in two columns "exambirulobin" and "calcium".
 
-pass
+data =  pd.read_csv('values.csv', sep=',', names=['exambirulobin', 'calcium'])
+data.head()
 
 # ### Exercise 6 (max 3 points)
 #
 #
 # Add to the dataframe two columns with the difference of the two measures in `values.csv` and the ratio of the difference and the *exambirulobin* value.
 
-pass
+data['diff'] = data['calcium'] - data['exambirulobin']
+data['ratio'] = data['diff'] / data['exambirulobin']
+data.head()
 
 # ### Exercise 7 (max 3 points)
 #
 # Make two adjacent plots: one with values of *exambirulobin* taken from `values.csv` and another with the histogram of the ratio computed in Exercise 6.
 
-pass
+# +
+t = np.linspace(0, 15, data['exambirulobin'].count())
+fig, ax = plt.subplots(1,2, figsize=(10,10))
+ax[0].plot(t, data['exambirulobin'], label='Exambirulobin')
+ax[1].hist(data['ratio'], bins='auto', density=True, label='ratio')
+
+_ = fig.legend()
+# -
 
 # ### Exercise 8 (max 5 points)
 #
 # Consider this statistical model: the ratio computed in Exercise 6 is normally distributed, with an unknown mean, and a standard deviation of 1. Your *a priori* estimation of the mean is a normal distribution with mean 0 and standard deviation 1. Use PyMC to sample the posterior distributions after having seen the actual values of the ratio.  Plot the results.
 
-pass
+# +
+mymodel = pm.Model()
+
+with mymodel:
+    mu = pm.Normal('mu', 0, 1)
+    
+    ratio = pm.Normal('ratio', mu, 1, observed=data['ratio'])
+    
+    post = pm.sample()
+
+# -
+
+with mymodel:
+    pm.plot_posterior(post)
 
 
